dcaa70b070690eeaf48afe0111d0e5ebbae68511
Luis Gustavo <gustascrows@gmail.com>
2021-01-26 10:40:01 -0300

    Assets: Styles do projeto

--- src/Styles.js
+++ src/Styles.js
@@ -2,28 +2,35 @@ import styled from 'styled-components/native';
 
 export const View = styled.SafeAreaView`
   flex: 1;
-  background-color: #fff;
+  background-color: #333;
   align-items: center;
   justify-content: center;
+  padding-top:40px;
 `;
 
 export const Text = styled.Text`
-
+font-size:24px;
+text-align:center;
+color:#fff;
+font-weight:bold;
 `;
 
-export const Button = styled.Button`
-
-`;
 
 export const FlatList = styled.FlatList`
-
+flex:1;
 `;
 
 export const Image = styled.Image`
-width:200px;
-height:200px;
+height:400px;
 `;
 
 export const ViewArea = styled.View`
+margin-bottom:40px;
+margin-top:40px;
+`;
 
+export const LoadingArea = styled.View`
+flex:1;
+justify-content:center;
+align-items:center;
 `;
